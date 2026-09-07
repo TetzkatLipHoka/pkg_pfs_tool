7bb74a6f4212a90afb63c1f1b80e3a56635c768c
TetzkatLipHoka <TLH@freenet.de>
2026-09-07 19:17:17 +0200

    Delete src directory

--- src/config.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "common.h"
-
-typedef int (*parse_config_cb_t)(void* arg, const char* section, const char* name, const char* value);
-
-int parse_config_file(const char* path, parse_config_cb_t cb, void* arg);
